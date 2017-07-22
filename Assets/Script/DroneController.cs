1fddee7beda9b23093f8a3e0471cb8ffccc54077
yuzuka4573 <yuto457350@gmail.com>
2017-07-23 01:11:41 +0900

    fix drone rotation movement

--- Assets/Script/DroneController.cs
+++ Assets/Script/DroneController.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement; 
 
 public class DroneController : MonoBehaviour {
 
 	GameObject drone;
 	Rigidbody rb;
+	public int speedval = 30;
+	public int UDval = 100;
+	public float rotateval=1.5f;
+	public float LCRval = 0f;
+	public float RCRval = 0f;
 	// Use this for initialization
 	void Start () {
 		drone = GameObject.Find ("Drone_test_edited");
@@ -16,38 +22,56 @@ public class DroneController : MonoBehaviour {
 
 		//moving
 		//x and z section
-		if (Input.GetAxis("Horizontal") != 0f) {
-			rb.AddForce (gameObject.transform.right*30*Input.GetAxis("Horizontal"), ForceMode.Force);
+		if (Input.GetAxis ("Horizontal") != 0f) {
+			rb.AddForce (gameObject.transform.right * speedval * Input.GetAxis ("Horizontal"), ForceMode.Force);
 		}
-		if (Input.GetAxis("Vertical") != 0f) {
-			rb.AddForce (gameObject.transform.forward*30*Input.GetAxis("Vertical"), ForceMode.Force);
+		if (Input.GetAxis ("Vertical") != 0f) {
+			rb.AddForce (gameObject.transform.forward * speedval * Input.GetAxis ("Vertical"), ForceMode.Force);
 		}
 
 		// y section
-		if (Input.GetKey("joystick button 4")||Input.GetKey(KeyCode.Space)) {
+		if (Input.GetKey ("joystick button 4") || Input.GetKey (KeyCode.Space)) {
 			Debug.Log ("Press up");
-			rb.AddForce (gameObject.transform.up*100, ForceMode.Force);
+			rb.AddForce (gameObject.transform.up * UDval, ForceMode.Force);
 		}
-		/*if (Input.GetKey("joystick button 5")||Input.GetKey("k")) {
-			Debug.Log ("Press down");
-			rb.AddForce (gameObject.transform.up*-100,  ForceMode.Force);
-		}*/
+		if (SceneManager.GetActiveScene ().name != "Hardmode") {
+
+			if (Input.GetKey ("joystick button 5") || Input.GetKey ("c")) {
+				Debug.Log ("Press down");
+				rb.AddForce (gameObject.transform.up * -UDval, ForceMode.Force);
+			}
+		}
 		//break section
-		if (Input.GetKeyDown("joystick button 8")) {
+		if (Input.GetKeyDown("joystick button 8")||Input.GetKey("i")) {
 			Debug.Log("move break");
 			rb.drag = 3;
 			StartCoroutine (stopMove ());
 		}
-		if (Input.GetKey("i")) {
-			Debug.Log("move break");
-			rb.drag = 3;
-			StartCoroutine (stopMove ());
-		}
-		if (Input.GetKeyDown("joystick button 9")) {
+		if (Input.GetKeyDown("joystick button 9") ||Input.GetKey ("k")) {
 			Debug.Log("rotate break");
 			rb.angularDrag = 3;
 			StartCoroutine (stopRotate ());
 		}
+		//rotation
+		if (Input.GetAxis("rotateY") != 0f) {
+			Debug.Log ("rotate");
+			rb.angularVelocity=new Vector3(0f,Input.GetAxis("rotateY")*rotateval,0f);
+		}
+		if (Input.GetKey ("j")) {
+			Debug.Log ("rotate");
+			if(LCRval<rotateval)LCRval += 0.01f;
+			if (RCRval > 0)RCRval -= 0.01f;
+			rb.angularVelocity = new Vector3 (0f, -1 * (LCRval-RCRval), 0f);
+		} else if (Input.GetKey ("l")) {
+			Debug.Log ("rotate");
+			if(RCRval<rotateval)RCRval += 0.01f;
+			if (LCRval > 0)LCRval -= 0.01f;
+			rb.angularVelocity = new Vector3 (0f, (RCRval-LCRval), 0f);
+		} else {
+			if (LCRval > 0)LCRval -= 0.01f;
+			if (RCRval > 0)RCRval -= 0.01f;
+		}
+
 		/*if (Input.GetKeyDown ("joystick button 9")) {
 			Debug.Log ("Break all");
 			rb.angularDrag = 3;
@@ -59,22 +83,6 @@ public class DroneController : MonoBehaviour {
 		float inputy = Input.GetAxis ("rotateY");
 		rb.rotation = transform.rotation * Quaternion.Euler (0, inputy, 0);
 		*/
-		if (Input.GetAxis("rotateY") != 0f) {
-			Debug.Log ("rotate");
-			rb.angularVelocity=new Vector3(0f,Input.GetAxis("rotateY")*1.5f,0f);
-		}
-		if (Input.GetKey ("j")) {
-			Debug.Log ("rotate");
-			rb.angularVelocity = new Vector3 (0f, -1.5f, 0f);
-		} else if (Input.GetKey ("l")) {
-			Debug.Log ("rotate");
-			rb.angularVelocity = new Vector3 (0f, 1.5f, 0f);
-		} else if (Input.GetKey ("k")) {
-			Debug.Log("rotate break");
-			rb.angularDrag = 3;
-			StartCoroutine (stopRotate ());
-		}
-
 	}
 
 	private IEnumerator stopMove(){
